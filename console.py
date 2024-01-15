73ebd88e102637d95883871680345789da43c565
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-01-15 04:58:00 +0100

    Started AirBnb5

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Command line Interpreter like shell in c"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    """AirBnb Command line interpreter"""
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        """ Signals end of File """
+        return True
+
+    def do_help(self, arg):
+        """ Signals sent for help """
+        cmd.Cmd.do_help(self, arg)
+
+    def do_quit(self, arg):
+        """Exits console"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
